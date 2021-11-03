6523d402861ced93cf438e01eaa3d2abd6c92f56
Bui Ngoc Thanh <thanhbn87@gmail.com>
2021-11-03 15:57:12 +0700

    Update cloudfront_data

--- cloudfront_data/outputs.tf
+++ cloudfront_data/outputs.tf
@@ -1,9 +1,9 @@
 output "cloudfront_cache_policy_id" {
-  description = "The origin access identities created."
+  description = "The identifier for the cache policy."
   value       = data.aws_cloudfront_cache_policy.this.id
 }
 
 output "cloudfront_request_policy_id" {
-  description = "The IDS of the origin access identities created."
+  description = "The identifier for the origin request policy."
   value       = data.aws_cloudfront_origin_request_policy.this.id
 }
--- cloudfront_data/variables.tf
+++ cloudfront_data/variables.tf
@@ -1,11 +1,11 @@
 variable "cache_policy_name" {
   description = "A unique name to identify the cache policy."
   type        = string
-  default     = "CachingDisabled"
+  default     = "Managed-CachingDisabled"
 }
 
 variable "request_policy_name" {
   description = "Unique name to identify the origin request policy."
   type        = string
-  default     = "AllViewer"
+  default     = "Managed-AllViewer"
 }
