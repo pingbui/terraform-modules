b2c570a0be41e88a28a3eb2d78458917d4750871
Bui Ngoc Thanh <thanhbn87@gmail.com>
2021-11-03 15:27:52 +0700

    Add cloudfront_data

--- cloudfront_data/outputs.tf
+++ cloudfront_data/outputs.tf
@@ -1,9 +1,9 @@
 output "cloudfront_cache_policy_id" {
   description = "The origin access identities created."
-  value       = aws_cloudfront_cache_policy.this.id
+  value       = data.aws_cloudfront_cache_policy.this.id
 }
 
 output "cloudfront_request_policy_id" {
   description = "The IDS of the origin access identities created."
-  value       = aws_cloudfront_origin_request_policy.this.id
+  value       = data.aws_cloudfront_origin_request_policy.this.id
 }
