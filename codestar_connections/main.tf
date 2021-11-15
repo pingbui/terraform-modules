b174f5af214288f69ab9522ff738c94786a98fa0
Bui Ngoc Thanh <thanhbn87@gmail.com>
2021-11-15 16:15:26 +0700

    Change into resource

--- codestar_connections/main.tf
+++ codestar_connections/main.tf
@@ -1,4 +1,4 @@
-data "aws_codestarconnections_connection" "this" {
+resource "aws_codestarconnections_connection" "this" {
   name = var.codestarconnections_name
   provider_type = var.codestarconnections_provider
 }
