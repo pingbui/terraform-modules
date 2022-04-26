6c301e58bde5af6593fc4181778c0cb603d04568
Bui Ngoc Thanh <thanhbn87@gmail.com>
2022-04-26 09:02:57 +0700

    Add google_project_service

--- gcp-data/main.tf
+++ gcp-data/main.tf
@@ -1,23 +1,34 @@
+resource "google_project_service" "project" {
+  service = "compute.googleapis.com"
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_dependent_services = true
+}
+
 data "google_compute_regions" "selected" {}
 
 data "google_compute_zones" "available" {}
 
 data "google_compute_image" "ubuntu_1804_minimal" {
-  family = "ubuntu-minimal-1804-lts"
+  family  = "ubuntu-minimal-1804-lts"
   project = "ubuntu-os-cloud"
 }
 
 data "google_compute_image" "ubuntu_2004" {
-  family = "ubuntu-2004-lts"
+  family  = "ubuntu-2004-lts"
   project = "ubuntu-os-cloud"
 }
 
 data "google_compute_image" "centos7" {
-  family = "centos-7"
+  family  = "centos-7"
   project = "centos-cloud"
 }
 
 data "google_compute_image" "centos8" {
-  family = "centos-8"
+  family  = "centos-8"
   project = "centos-cloud"
 }
