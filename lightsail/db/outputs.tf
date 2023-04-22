5423ea8e56bd87f6c475dd2266ad1f92d17430d9
Thanh Bui Ngoc <thanhbn87@gmail.com>
2023-04-22 21:24:24 +0700

    Add output master_passwd

--- lightsail/db/outputs.tf
+++ lightsail/db/outputs.tf
@@ -2,3 +2,9 @@ output "id" {
   value       = aws_lightsail_database.this.id
   description = "The ARN of the Lightsail instance."
 }
+
+output "master_password" {
+  value       = local.master_password
+  description = "The password of the db."
+  sensitive   = true
+}
